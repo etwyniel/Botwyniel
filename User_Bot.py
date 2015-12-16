4e6fc61279757fb1960fcf905f5d4e8b8def9a18
Etwyniel <beringer.aymeric@laposte.net>
2015-12-16 20:05:28 +0100

    Added custom bot file.
    ... instead of using my own bot account.

--- User_Bot.py
+++ User_Bot.py
@@ -8,45 +8,52 @@ from RiotAPI import log
 # Set up the logging module to output diagnostic to the console.
 logging.basicConfig()
 
-botwyniel = Bot('etwyspam@gmail.com', 'almg0308')
-botwyniel.log(0)
+name = input("Bot name: ")
+email = input("Bot email: ")
+password = input("Bot password: ")
+
+user_bot = Bot(email, password, name)
+system("cls")
+
+user_bot.log(0)
 system('title Botwyniel')
 
-if not botwyniel.is_logged_in:
+if not user_bot.is_logged_in:
     print("Logging in to Discord failed")
     exit(1)
 
 
-@botwyniel.event
+@user_bot.event
 def on_message(message):
     if message.content.startswith('!'):
         log(str(message.author) + ": " + message.content)
-        if message.content.split(' ')[0] in botwyniel.commands:
-            botwyniel.commands[message.content.split(" ")[0]](message)
+        if message.content.split(' ')[0] in user_bot.commands:
+            user_bot.commands[message.content.split(" ")[0]](message)
 
 
-@botwyniel.event
+@user_bot.event
 def on_ready():
     log("Botwyniel initialized")
-    botwyniel.change_status(443)
+    user_bot.change_status(443)
+
     system("cls")
-    print('Logged in as ' + botwyniel.user.name)
+    print('Logged in as ' + user_bot.user.name)
 
     print("\nAvailable servers:")
-    for a in botwyniel.servers:
+    for a in user_bot.servers:
         print(a.name)
-        botwyniel.servs[a.name] = a
+        user_bot.servs[a.name] = a
         ch_dict = {}
         for channel in a.channels:
             ch_dict[channel.name] = channel
-        botwyniel.channels[a] = ch_dict
+        user_bot.channels[a] = ch_dict
     print('------')
 
 
-@botwyniel.event
+@user_bot.event
 def on_member_join(user):
     log(user.name + ' has joined ' + user.server.name)
     print(user.name + ' has joined ' + user.server.name)
 
 sleep(0.5)
-botwyniel.run()
+user_bot.run()
