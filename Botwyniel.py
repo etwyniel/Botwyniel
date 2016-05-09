9eeb3b0fd04a92dfb429204c00288fdab7e3212b
etwyniel <beringer.aymeric@laposte.net>
2016-05-09 23:02:57 +0200

    Should now work on Heroku's Linux environment

--- Botwyniel.py
+++ Botwyniel.py
@@ -507,7 +507,7 @@ class Bot(discord.Client):
         await self.send_message(channel, to_send)
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus('libopus.so.0.5.0')
     
 botwyniel = Bot(wl=["Etwyniel", "Jhysodif"])
 botwyniel.run(os.environ['DISCORD_TOKEN'])
