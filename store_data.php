447c355cec1c5192ac5b5a94993f8df0802bd8db
etwyniel <aymeric.beringer@epita.fr>
2016-09-14 18:09:25 +0200

    Update store_data.php

--- store_data.php
+++ store_data.php
@@ -6,17 +6,21 @@
     $db_password = substr($db_url, strpos($db_url, ":1") + 1, strpos($db_url, "@") - strpos($db_url, ":1") - 1);
     $db_database = substr($db_url, strpos($db_url, "/h") + 1, strpos($db_url, "?") - strpos($db_url, "/h") - 1);
     
+    $key = $_POST['key'];
+    $value = $_POST['value'];
+    
     echo $db_database . "<br>";
     
     $mysql = new mysqli($db_server, $db_username, $db_password, $db_database);
     
-    $query = "INSERT INTO botwyniel_data (name, val) VALUES ('last update', '6.17');";
+    $query = "INSERT INTO botwyniel_data (name, val) VALUES ('" . $key ."', '" . $value . "');";
     if ($mysql->query($query)) {
         echo "Values added successfully.<br>";
     } else {
         die("Failed to add values: " . $mysql->error);
     }
     $mysql->close();
+    /*
     for ($a = 0; $a <= $r->num_rows - 2; $a++) {
         $user = $r->fetch_assoc();
         echo '"' . $user['id'] . '":{';
@@ -35,4 +39,5 @@
     echo ', "discriminator":"' . $user['discriminator'];
     echo '", "avatar":"' . $user['avatar'];
     echo '"}}';
+    */
 ?>
