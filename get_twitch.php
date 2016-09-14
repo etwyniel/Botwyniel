56f422f0dfff59cac574d22628fc5dba547519a2
etwyniel <aymeric.beringer@epita.fr>
2016-09-14 17:26:53 +0200

    Update get_twitch.php

--- get_twitch.php
+++ get_twitch.php
@@ -4,10 +4,16 @@
     $db_server = substr($db_url, strpos($db_url, "@") + 1, strpos($db_url, "/h") - strpos($db_url, "@") - 1);
     $db_username = substr($db_url, strpos($db_url, "//") + 2, strpos($db_url, ":1") - strpos($db_url, "//") - 2);
     $db_password = substr($db_url, strpos($db_url, ":1") + 1, strpos($db_url, "@") - strpos($db_url, ":1") - 1);
-    $mysql = new mysqli($db_server, $db_username, $db_password, $db_username);
-    $query = "SELECT * FROM twitch_follow";
-    $r = $mysql->query($query);
-    echo "{";
+    
+    $mysql = new mysqli_conn($db_server, $db_username, $db_password, $db_username);
+    
+    $query = "CREATE TABLE IF NOT EXISTS botwyniel_data (key VARCHAR(32)) ";
+    if ($mysql->query($query)) {
+        echo "Table created successfully.<br>";
+    } else {
+        die("Failed to create table: " . $mysql->error);
+    }
+    $mysql->close();
     for ($a = 0; $a <= $r->num_rows - 2; $a++) {
         $user = $r->fetch_assoc();
         echo '"' . $user['id'] . '":{';
