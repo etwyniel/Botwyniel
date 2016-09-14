70e49e1aa80b6568f621c5016ddaf8f82dd1ce49
etwyniel <aymeric.beringer@epita.fr>
2016-09-14 13:56:41 +0200

    Update get_twitch.php

--- get_twitch.php
+++ get_twitch.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <?php
     $db_url = $_ENV['CLEARDB_DATABASE_URL'];
-    $db_server = substr($db_url, strpos($db_url, "@") + 1, strpos($db_url, "/h") - strpos($db_url, "@"));
+    $db_server = substr($db_url, strpos($db_url, "@") + 1, strpos($db_url, "/h") - strpos($db_url, "@") - 1);
     $db_username = substr($db_url, strpos($db_url, "//") + 2, strpos($db_url, ":1") - strpos($db_url, "//") - 2);
     $db_password = substr($db_url, strpos($db_url, ":1") + 1, strpos($db_url, "@") - strpos($db_url, ":1") - 1);
-    //TEMPORARY !!
-    echo $db_server . "<br>";
-    echo $db_username . "<br>";
-    echo $db_password . "<br>";
     $mysql = new mysqli($db_server, $db_username, $db_password, $db_username);
     $query = "SELECT * FROM twitch_follow";
     $r = $mysql->query($query);
