be1a3f7a5ddf2d7178e9692a68bd47e9b3cbf2db
etwyniel <aymeric.beringer@epita.fr>
2016-12-03 22:45:46 +0100

    Update Botwyniel.py

--- Botwyniel.py
+++ Botwyniel.py
@@ -162,7 +162,7 @@ class Bot(discord.Client):
 	
     async def join_voice(self, message):
         channel = self.truncate(message.content).lower()
-        for c in message.server.channel:
+        for c in message.server.channels:
             if c.name.lower == channel and c.type == "voice":
                 self.voice += await self.join_voice_channel(c)
                 self.voice[-1].player = None
